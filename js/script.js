be6e24e31f936ea02ec2a0378ba4b47d3b124cd3
Gunther Haas <guntherhaas.eng@gmail.com>
2023-06-08 22:02:07 -0300

    button that shows each list item

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let proximoItem = 1;
+
 const cartaoTitulo = document.querySelector('.cartao-titulo');
     cartaoTitulo.addEventListener('click', () => {
       cartaoTitulo.style.display = 'none';
@@ -7,3 +9,16 @@ const cartaoTitulo = document.querySelector('.cartao-titulo');
         cardPlanet.style.display = 'block';
       });
     });
+
+    
+    function mostrarProximo() {
+      const lista = document.getElementById("lista");
+      const proximoLi = lista.querySelector(`li:nth-child(${proximoItem})`);
+      if (proximoLi) {
+        proximoLi.style.display = "list-item";
+        proximoItem++;
+        if (!proximoLi.nextElementSibling) {
+          document.querySelector("button").style.display = "none";
+        }
+      }
+    }
